cebe58fb22a52f4447f0834e1d3779274e773880
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-01-25 15:55:03 +0800

    add

--- photo-editor-demo2/js/main.js
+++ photo-editor-demo2/js/main.js
@@ -5,8 +5,8 @@ const canvasCtx = canvas.getContext("2d");
 const resetBtn = document.querySelector(".reset");
 
 resetBtn.onclick = function () {
-  resetSettings();
-  renderImage();
+    resetSettings();
+    renderImage();
 };
 
 const brightnessInput = document.querySelector("#brightness");
@@ -15,66 +15,115 @@ const blurInput = document.querySelector("#blur");
 const inversionInput = document.querySelector("#inversion");
 
 const settings = {
-  brightness,
-  saturation,
-  blur,
-  inversion,
+    brightness,
+    saturation,
+    blur,
+    inversion,
 };
 let image = null;
 
 function resetSettings() {
-  settings.brightness = "100";
-  settings.saturation = "100";
-  settings.blur = "0";
-  settings.inversion = "0";
+    settings.brightness = "100";
+    settings.saturation = "100";
+    settings.blur = "0";
+    settings.inversion = "0";
 
-  brightnessInput.value = settings.brightness;
-  saturationInput.value = settings.saturation;
-  blurInput.value = settings.blur;
-  inversionInput.value = settings.inversion;
+    brightnessInput.value = settings.brightness;
+    saturationInput.value = settings.saturation;
+    blurInput.value = settings.blur;
+    inversionInput.value = settings.inversion;
 }
 
 function updateSettings(key, value) {
-  if (!image) return;
-  settings[key] = value;
-  renderImage();
+    if (!image) return;
+    settings[key] = value;
+    renderImage();
 }
 
 function generateFilter() {
-  const { brightness, saturation, blur, inversion } = settings;
-  return `brightness(${brightness}%) saturate(${saturation}%) blur(${blur}px) invert(${inversion}%)`;
+    const {brightness, saturation, blur, inversion} = settings;
+    return `brightness(${brightness}%) saturate(${saturation}%) blur(${blur}px) invert(${inversion}%)`;
 }
 
 function renderImage() {
-  canvas.width = image.width;
-  canvas.height = image.height;
+    canvas.width = image.width;
+    canvas.height = image.height;
 
-  canvasCtx.filter = generateFilter();
-  canvasCtx.drawImage(image, 0, 0);
+    canvasCtx.filter = generateFilter();
+    canvasCtx.drawImage(image, 0, 0);
 }
 
 brightnessInput.addEventListener("change", () =>
-  updateSettings("brightness", brightnessInput.value)
+    updateSettings("brightness", brightnessInput.value)
 );
 saturationInput.addEventListener("change", () => {
-  updateSettings("saturation", saturationInput.value);
+    updateSettings("saturation", saturationInput.value);
 });
 blurInput.addEventListener("change", () => {
-  updateSettings("blur", blurInput.value);
+    updateSettings("blur", blurInput.value);
 });
 inversionInput.addEventListener("change", () => {
-  updateSettings("inversion", inversionInput.value);
+    updateSettings("inversion", inversionInput.value);
 });
 
 fileInput.addEventListener("change", () => {
-  image = new Image();
+    image = new Image();
 
-  image.addEventListener("load", () => {
-    resetSettings();
-    renderImage();
-  });
+    image.addEventListener("load", () => {
+        resetSettings();
+        renderImage();
+    });
 
-  image.src = URL.createObjectURL(fileInput.files[0]);
+    image.src = URL.createObjectURL(fileInput.files[0]);
 });
+
+function removebackground() {
+    // let images = document.getElementById('urls').value.split('\n')
+
+    // images.forEach(imageUrl => {
+    const image = new Image();
+    image.onload = ({target}) => {
+        const w = Math.round(target.width);
+        const h = Math.round(target.height);
+
+        const canvas = document.createElement("canvas");
+        canvas.width = w;
+        canvas.height = h;
+        // const canvasCtx = canvas.getContext("2d");
+        // canvasCtx.drawImage(
+        //     target,
+        //     0,
+        //     0,
+        //     target.width,
+        //     target.height,
+        //     0,
+        //     0,
+        //     w,
+        //     h
+        // );
+
+        const canvasImageData = canvasCtx.getImageData(0, 0, w, h);
+
+        for (
+            let index = 0, dataLength = canvasImageData.data.length;
+            index < dataLength;
+            index += 4
+        ) {
+            const r = canvasImageData.data[index];
+            const g = canvasImageData.data[index + 1];
+            const b = canvasImageData.data[index + 2];
+            if ([r, g, b].every((item) => item > 230))
+                canvasImageData.data[index + 3] = 0;
+        }
+
+        target.width = w;
+        target.height = h;
+        canvasCtx.putImageData(canvasImageData, 0, 0);
+        // document.body.append(image, canvas);
+    };
+    image.crossOrigin = "";
+    image.src = URL.createObjectURL(fileInput.files[0]);
+    // });
+}
 
 resetSettings();
