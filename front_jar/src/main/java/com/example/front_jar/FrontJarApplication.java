eb961c3be7b41b52536b89b77b2acb1e8bf22e8d
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-03-15 22:31:25 +0800

    update

---
+++ front_jar/src/main/java/com/example/front_jar/FrontJarApplication.java
@@ -0,0 +1,13 @@
+package com.example.front_jar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontJarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontJarApplication.class, args);
+    }
+
+}
---
+++ front_jar/src/test/java/com/example/front_jar/FrontJarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.front_jar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontJarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
