561a55b524e3aef56944e8b71bf61ef11c044b0c
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-04-17 16:23:27 +0800

    update

--- sprint4/idphotomaker-master/vue.config.js
+++ sprint4/idphotomaker-master/vue.config.js
@@ -26,7 +26,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8081',//代理地址，这里设置的地址会代替axios中设置的baseURL
+        target: 'http://localhost:8085',//代理地址，这里设置的地址会代替axios中设置的baseURL
         changeOrigin: true,// 如果接口跨域，需要进行这个参数配置
         //ws: true, // proxy websockets
         //pathRewrite方法重写url
